488e8378dbbec59f6204071c05f0d31c970a17a5
kent28808 <kktanoue@gmail.com>
2020-11-18 15:29:26 -0800

    Removed full stack stuff

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+    <nav className="navbar navbar-light bg-light fixed-top">
+      <a className="navbar-brand" href="#">Navbar</a>
+    </nav>
+    )
+}
+
+export default Header;
